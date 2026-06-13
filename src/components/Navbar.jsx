4b7a4835522864e759a78957ee8e3eb6767a168b
Shivam Kumar <[singhraunak01003@gmail.com]>
2026-06-13 22:56:37 +0530

    Initial project setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center px-12 py-6">
+        <h1 className="text-red-600 text-4xl font-bold">
+            NETFLIX
+        </h1>
+        <button className="bg-red-600 text-white px-4 py-2 rounded">
+            Sign In
+        </button>
+    </nav>
+    
+  );
+}
+
+export default Navbar;
